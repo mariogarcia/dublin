53eff93d2b2233eef70c329e2d9d893d06d3552d
Mario Garcia <mario.ggar@gmail.com>
2013-11-09 18:51:05 +0100

    Added Tech-Compatibility-Kit module to make testing upcoming filesystems a little bit easier

--- dublin-tck/src/main/groovy/dublin/tck/PathCompatibilitySpec.groovy
+++ dublin-tck/src/main/groovy/dublin/tck/PathCompatibilitySpec.groovy
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dublin.temp
+package dublin.tck
 
 import java.nio.file.Paths
 import java.nio.file.Files
