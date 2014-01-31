3b6f1b74d16395f17e2aa04df1ec71e4e1fc2392
Mario Garcia <mario.ggar@gmail.com>
2014-02-01 00:16:38 +0100

    Refactoring HttpPathSpec to investigate about ways of creating a HttpPath

--- dublin-http/src/test/groovy/dublin/http/HttpPathSpec.groovy
+++ dublin-http/src/test/groovy/dublin/http/HttpPathSpec.groovy
@@ -1,13 +1,20 @@
 package dublin.http
 
-import java.net.URI;
+import static java.nio.file.Paths.get
+import static java.nio.file.Files.copy
+import static java.nio.file.Files.createTempDirectory
 
+import java.net.URI
 import java.nio.file.Path
-import java.nio.file.Paths
-import java.nio.file.Files
+import java.nio.file.FileSystems
 
 import dublin.tck.PathCompatibilitySpec
 
+/**
+ * Test to cover HttpPath functionality using the
+ * PathCompatibilitySpec
+ *
+ */
 class HttpPathSpec extends PathCompatibilitySpec {
 
     def setup() {
@@ -16,25 +23,19 @@ class HttpPathSpec extends PathCompatibilitySpec {
     }
 
     def 'Get a given resource from an URL'() {
-        given: 'A path'
-            HttpFileSystem httpFileSystem = createFileSystem()
-            Path google = new HttpPath(new URI('http://www.google.com'), httpFileSystem)
-            Path tmpFile =
-                Paths.get(
-                    Files.createTempDirectory('dublin').toString(),
-                    Paths.get('google.html').toString()
-                )
+        given: 'A file system'
+            URI googleURI = URI.create('http://www.google.com')
+            HttpFileSystem httpFileSystem = FileSystems.getFileSystem(googleURI)
+        and: 'A couple of paths for copying information to each other'
+            Path google = httpFileSystem.getPath(googleURI.toString())
+            Path tmpFile = get(
+                createTempDirectory('dublin').toString(),
+                get('google.html').toString()
+            )
         when: 'Copying an online resource to a local file'
-            Files.copy(google, tmpFile)
+            copy(google, tmpFile)
         then: 'We should have copied the resource from a remote url'
             tmpFile.toFile().exists()
     }
-
-    // def 'Copying from http to tmp'() {
-    //     given: 'A path'
-    //         Path google = from('http://www.google.com').to('tmp://dublin/google.html')
-    //     expect: 'We should have copied the resource from a remote url'
-    //         google.toFile().exists()
-    // }
 
 }
