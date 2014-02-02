86b5b65b61db9cf323e5344a4c041bcf4633633a
Mario Garcia <mario.ggar@gmail.com>
2014-02-02 14:34:54 +0100

    Default JDK system provider has a funny way of resolving URIs containing their scheme part

--- dublin-core/src/main/java/dublin/core/Dublin.java
+++ dublin-core/src/main/java/dublin/core/Dublin.java
@@ -8,15 +8,58 @@ import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 
 
+/**
+ * This class is intended to be an entry point for resolving Path instances no matter
+ * the file system they have to be resolve against.
+ *
+ * This should be the job java.nio.file.Paths should be doing but unfortunately
+ * current version only resolves paths based on the local file system.
+ *
+ * @author Mario Garcia
+ * @since version 1.0.0
+ *
+ */
 public final class Dublin {
 
+    /**
+     * The UnixFileSystemProvider expects just the part
+     * identifying the file system. If you oass a
+     * complete URI it will complain throwing an exception
+     */
+    private static final String JDK_FILE_PROVIDER_HACK = ":/";
+
+    /**
+     * This method resolves a given file system by the URI passed as
+     * argument. It will try to resolve the file system from the
+     * scheme within the URI and once the FS has been acquired then
+     * the path is resolved
+     *
+     * @param URI uri The uri we want to resolve
+     * @return the resolved path
+     */
     public static Path from(URI uri) {
-        FileSystem fileSystem = FileSystems.getFileSystem(uri);
-        Path path = fileSystem.getPath(uri.toString());
+
+        String scheme = uri.getScheme();
+        URI fsURI = URI.create(scheme + JDK_FILE_PROVIDER_HACK);
+     /* In order to get the file system we only pass the scheme plus
+      * the minimum required info */
+        FileSystem fileSystem = FileSystems.getFileSystem(fsURI);
+
+     /* Once we get the file system we can resolve the full path */
+        Path path = fileSystem.getPath(uri.toString().substring(5));
 
         return path;
     }
 
+    /**
+     * This method resolves a given file system by the String passed as
+     * argument. It will try to resolve the file system from the
+     * scheme within the URI and once the FS has been acquired then
+     * the path is resolved
+     *
+     * @param String uri The uri we want to resolve
+     * @return the resolved path
+     */
     public static Path from(String uri) {
         return from(URI.create(uri));
     }
--- dublin-core/src/test/groovy/dublin/core/DublinSpec.groovy
+++ dublin-core/src/test/groovy/dublin/core/DublinSpec.groovy
@@ -3,6 +3,7 @@ package dublin.core;
 import static dublin.core.Dublin.from;
 
 import java.nio.file.Path
+import spock.lang.Unroll
 import spock.lang.Specification
 
 /**
@@ -10,17 +11,23 @@ import spock.lang.Specification
  */
 class DublinSpec extends Specification {
 
-    def 'Building a path from both a String and an URI'() {
+    @Unroll
+    def 'Building a path from both a String and an URI [#path]'() {
         when: 'Building both paths from different sources'
-            String src = 'file:///'
-            String file = new File('./src/test/resources/file.txt').absolutePath
-            URI sourceURI = URI.create(src)
-            Path simplePathWithURI = from(sourceURI).resolve(file)
-            Path simplePathWithString = from(src).resolve(file)
-            println simplePathWithString.toFile().absolutePath
-        then: 'Both paths are the same'
-            simplePathWithString.toFile().exists()
-            simplePathWithURI.toFile().exists()
+            Path simplePath = from(path)
+        then: 'Both paths can be resolved properly'
+            simplePath.toFile().exists()
+        where:
+            path << [fileURI, fileURIToString]
     }
+
+    URI getFileURI() {
+        return new File("src/test/resources/file.txt").toURI()
+    }
+
+    String getFileURIToString() {
+        return fileURI.toString()
+    }
+
 
 }
